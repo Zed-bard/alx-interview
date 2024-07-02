f7050fa8d7bbdc96096a892493fc79951a2635cd
Zed-bard <zedbard@gmail.com>
2024-07-02 01:44:57 -0700

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,32 +3,18 @@
 '''
 
 def canUnlockAll(boxes):
-    '''Determines if all boxes can be unlocked.
-
-    Each box may contain keys to other boxes. This function checks if it is
-    possible to unlock all the boxes, starting from the first box (box 0).
-
-    Args:
-        boxes (list of list of int): A list where each index represents a box 
-                                     and each element is a list of keys 
-                                     contained in that box.
-
-    Returns:
-        bool: True if all boxes can be unlocked, False otherwise.
+    '''Checks if all the boxes in a list of boxes containing the keys
+    (indices) to other boxes can be unlocked given that the first
+    box is unlocked.
     '''
     n = len(boxes)
-    if n == 0:
-        return True  # No boxes to unlock
-
     seen_boxes = set([0])
     unseen_boxes = set(boxes[0]).difference(set([0]))
-
-    while unseen_boxes:
+    while len(unseen_boxes) > 0:
         boxIdx = unseen_boxes.pop()
-        if boxIdx >= n or boxIdx < 0:
-            continue  # Skip invalid indices
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
         if boxIdx not in seen_boxes:
             unseen_boxes = unseen_boxes.union(boxes[boxIdx])
             seen_boxes.add(boxIdx)
-
     return n == len(seen_boxes)

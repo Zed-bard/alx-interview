5ded7214ea5e27e0d0918589e3394aaec60cda4c
root <zerihunbard@gmail.com>
2024-07-31 05:54:34 -0700

    0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -21,12 +21,14 @@ def get_input():
     Returns:
         int: The size of the chessboard.
     """
+    global n
+    n = 0
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
     try:
         n = int(sys.argv[1])
-    except ValueError:
+    except Exception:
         print("N must be a number")
         sys.exit(1)
     if n < 4:
@@ -45,7 +47,7 @@ def is_attacking(pos0, pos1):
     Returns:
         bool: True if the queens are in an attacking position else False.
     """
-    if pos0[0] == pos1[0] or pos0[1] == pos1[1]:
+    if (pos0[0] == pos1[0]) or (pos0[1] == pos1[1]):
         return True
     return abs(pos0[0] - pos1[0]) == abs(pos0[1] - pos1[1])
 
@@ -61,7 +63,12 @@ def group_exists(group):
     """
     global solutions
     for stn in solutions:
-        if all(any(stn_pos == grp_pos for stn_pos in stn) for grp_pos in group):
+        i = 0
+        for stn_pos in stn:
+            for grp_pos in group:
+                if stn_pos[0] == grp_pos[0] and stn_pos[1] == grp_pos[1]:
+                    i += 1
+        if i == n:
             return True
     return False
 
@@ -73,7 +80,8 @@ def build_solution(row, group):
         row (int): The current row in the chessboard.
         group (list of lists of integers): The group of valid positions.
     """
-    global solutions, n
+    global solutions
+    global n
     if row == n:
         tmp0 = group.copy()
         if not group_exists(tmp0):
@@ -81,12 +89,12 @@ def build_solution(row, group):
     else:
         for col in range(n):
             a = (row * n) + col
-            matches = zip([pos[a]] * len(group), group)
-            used_positions = map(lambda x: is_attacking(*x), matches)
+            matches = zip(list([pos[a]]) * len(group), group)
+            used_positions = map(lambda x: is_attacking(x[0], x[1]), matches)
+            group.append(pos[a].copy())
             if not any(used_positions):
-                group.append(pos[a].copy())
                 build_solution(row + 1, group)
-                group.pop()
+            group.pop(len(group) - 1)
 
 
 def get_solutions():
@@ -94,11 +102,12 @@ def get_solutions():
     """
     global pos, n
     pos = list(map(lambda x: [x // n, x % n], range(n ** 2)))
-    build_solution(0, [])
+    a = 0
+    group = []
+    build_solution(a, group)
 
 
 n = get_input()
 get_solutions()
 for solution in solutions:
     print(solution)
-
